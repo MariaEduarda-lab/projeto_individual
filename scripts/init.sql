dc05157fdcf66b2bf96343bc45f137619ffcea14
Maria Eduarda <mariaeduardabarbosah@gmail.com>
2025-05-09 09:30:25 -0300

    fix: correção do código em sql e suas relações com chaves estrangeiras

--- scripts/init.sql
+++ scripts/init.sql
@@ -1,3 +1,12 @@
+-- Criação da tabela dono_banca
+CREATE TABLE IF NOT EXISTS dono_banca (
+    id SERIAL PRIMARY KEY, -- chave de identificação
+    nome VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(100) NOT NULL,
+    telefone VARCHAR(20) NOT NULL
+);
+
 -- Criação da tabela freguês
 CREATE TABLE IF NOT EXISTS fregues (
     id SERIAL PRIMARY KEY, -- chave de identificação
@@ -5,36 +14,30 @@ CREATE TABLE IF NOT EXISTS fregues (
     email VARCHAR(100) NOT NULL UNIQUE, -- varchar permite escrita com palavras e números e o número delimita os caracteres máximos
     senha VARCHAR(100) NOT NULL,
     endereco VARCHAR(500) NOT NULL,
-    telefone VARCHAR(13) NOT NULL
-);
-
--- Criação da tabela dono_banca
-CREATE TABLE IF NOT EXISTS dono_banca (
-    id SERIAL PRIMARY KEY, -- chave de identificação
-    nome VARCHAR(100) NOT NULL,
-    email VARCHAR(100) NOT NULL UNIQUE,
-    senha VARCHAR(100) NOT NULL,
-    telefone VARCHAR(13) NOT NULL
+    telefone VARCHAR(20) NOT NULL,
+    dono_banca_id INT,
+    FOREIGN KEY (dono_banca_id) REFERENCES dono_banca(id)
 );
 
 -- Criação da tabela compra
 CREATE TABLE IF NOT EXISTS compra (
-    id SERIAL PRIMARY KEY NOT NULL,
+    id SERIAL PRIMARY KEY,
     data_pedido DATE NOT NULL,
     data_entrega DATE NOT NULL,
-    FOREIGN KEY id_fregues INT REFERENCES fregues(id) NOT NULL, -- chave estrangeira, que permite relação com a tabela de origem
-    FOREIGN KEY id_dono INT REFERENCES dono_banca(id) NOT NULL,
     valor_estimdo_total NUMERIC(10,2) NOT NULL, -- definição de inserção numérica som 10 dígitos máximos e 2 casas decimais (dinheiro)
     valor_final_total NUMERIC(10,2) NOT NULL,
-    aprovado_por_dono VARCHAR (20) NOT NULL
+    aprovado_por_dono VARCHAR (20) NOT NULL,
+    fregues_id INT,
+    FOREIGN KEY (fregues_id) REFERENCES fregues(id) -- chave estrangeira com relação na sua tabela de origem
 );
 
 -- Criação da tabela item_compra
 CREATE TABLE IF NOT EXISTS item_compra (
     id SERIAL PRIMARY KEY,
-    FOREIGN KEY id_compra INT REFERENCES compra(id),
     nome_produto VARCHAR(100) NOT NULL,
     quantidade NUMERIC NOT NULL,
     preco_estimado_unitario NUMERIC(10,2) NOT NULL,
-    subtotal_estimado NUMERIC(10,2) NOT NULL
+    subtotal_estimado NUMERIC(10,2) NOT NULL,
+    compra_id INT,
+    FOREIGN KEY (id_compra) REFERENCES compra(id)
 );
