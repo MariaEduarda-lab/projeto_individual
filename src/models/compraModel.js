df88db9dd35c1cbcc1955adcbeb0a1a9429969ff
Maria Eduarda <mariaeduardabarbosah@gmail.com>
2025-06-04 18:20:53 -0300

    fix: refinamento de scripts e models

--- src/models/compraModel.js
+++ src/models/compraModel.js
@@ -7,7 +7,7 @@ class CompraModel {
       data_entrega: Joi.date().optional(), 
       valor_estimado_total: Joi.number().precision(2).positive().required(),
       valor_final_total: Joi.number().precision(2).positive().optional(), 
-      status: Joi.boolean().required(),
+      status_pedido: Joi.boolean().required(),
       fregues_id: Joi.number().integer().required(),
       dono_banca_id: Joi.number().integer().required()
     });
--- src/models/itemCompraModel.js
+++ src/models/itemCompraModel.js
@@ -4,6 +4,7 @@ class ItemCompraModel {
   static get schema() {
     return Joi.object({
       quantidade: Joi.number().positive().required(),
+      subtotal_estimado: Joi.number().precision(2).positive().required(),
       subtotal_final: Joi.number().precision(2).positive().optional(),
       mercadoria_id: Joi.number().integer().required(),
       compra_id: Joi.number().integer().required()
