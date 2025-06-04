be3beba71d34c0263717c551cdcc4d56e659ccd8
Maria Eduarda <mariaeduardabarbosah@gmail.com>
2025-06-04 18:00:54 -0300

    fix: ajustar detalhes relacionais em models

--- src/models/compraModel.js
+++ src/models/compraModel.js
@@ -1,17 +1,17 @@
 const Joi = require('joi');
 
 class CompraModel {
-    static get schema() {
-        return Joi.object({
-            data_pedido: Joi.date().required(),
-            data_entrega: Joi.date().required(),
-            valor_estimado_total: Joi.number().precision(2).positive().required(),
-            valor_final_total: Joi.number().precision(2).positive().optional(),
-            status: Joi.boolean().required(),
-            fregues_id: Joi.number().integer().required(),
-            dono_banca_id: Joi.number().integer().required()
-        });
-    }
+  static get schema() {
+    return Joi.object({
+      data_pedido: Joi.date().required(),
+      data_entrega: Joi.date().optional(), 
+      valor_estimado_total: Joi.number().precision(2).positive().required(),
+      valor_final_total: Joi.number().precision(2).positive().optional(), 
+      status: Joi.boolean().required(),
+      fregues_id: Joi.number().integer().required(),
+      dono_banca_id: Joi.number().integer().required()
+    });
+  }
 }
 
 module.exports = CompraModel;
--- src/models/itemCompraModel.js
+++ src/models/itemCompraModel.js
@@ -1,16 +1,14 @@
 const Joi = require('joi');
 
 class ItemCompraModel {
-    static get schema() {
-        return Joi.object({
-            quantidade: Joi.number().positive().required(),
-            subtotal_estimado: Joi.number().precision(2).positive().required(),
-            valor_final_unitario: Joi.number().precision(2).positive().optional(),
-            subtotal_final: Joi.number().precision(2).positive().optional(),
-            mercadoria_id: Joi.number().integer().required(),
-            compra_id: Joi.number().integer().required()
-        });
-    }
+  static get schema() {
+    return Joi.object({
+      quantidade: Joi.number().positive().required(),
+      subtotal_final: Joi.number().precision(2).positive().optional(),
+      mercadoria_id: Joi.number().integer().required(),
+      compra_id: Joi.number().integer().required()
+    });
+  }
 }
 
 module.exports = ItemCompraModel;
